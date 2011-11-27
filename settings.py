25ab800d76ad98184864137a903ecb49c811164c
Jairo Suarez <slayerjairo@gmail.com>
2011-11-27 20:25:28 +0000

    adding local_settings.py and internationalization

--- settings.py
+++ settings.py
@@ -9,12 +9,12 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-     ('Jairo Suarez', 'slayerjairo@gmail.com'),
+     #('Your Name', 'email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-LANGUAGES = [('es', 'es')]
+LANGUAGES = [('en', 'en')]
 DEFAULT_LANGUAGE = 0
 
 DATABASES = {
@@ -31,11 +31,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Bogota'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'es-CO'
+LANGUAGE_CODE = 'es-us'
 
 SITE_ID = 1
 
@@ -57,18 +57,18 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = 'http://localhost/media/'
+MEDIA_URL = '/media/'
 
-STATIC_URL = 'http://localhost/static/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = 'http://localhost/static/admin/'
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'nUKEFpin6luH0R25JkVs*%DnhSgPqPoHOfrJChWlKN'
-
+SECRET_KEY = 'CHANGE_ME_PLEASE' #CHANGE THIS TO A LONGER STRING
+assert len(SECRET_KEY) > 16 , "you need to change the SECRET_KEY value to a longer string"
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -81,6 +81,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'cms.middleware.multilingual.MultilingualURLMiddleware', #Comment this if you are not going to use internationalization
     'cms.middleware.page.CurrentPageMiddleware',
     'cms.middleware.user.CurrentUserMiddleware',
     'cms.middleware.toolbar.ToolbarMiddleware',
@@ -131,3 +132,8 @@ INSTALLED_APPS = (
     'cms.plugins.googlemap',
     'sekizai',
 )
+# You can override any settings writing them on a local_setting.py
+try:
+    from local_settings import *
+except ImportError:
+    pass
